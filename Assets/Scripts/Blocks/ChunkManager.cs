812fc74972dad7e1a3064613ef4308e2185ebf5f
ZapperCZ <46494869+ZapperCZ@users.noreply.github.com>
2022-08-23 14:53:44 +0200

    Working on the chunk system
    Added functions to get position of a block / get block at a position
    Manually created 8 chunks for debug purposes

--- Assets/Scripts/Blocks/ChunkManager.cs
+++ Assets/Scripts/Blocks/ChunkManager.cs
@@ -10,11 +10,35 @@ public class ChunkManager : MonoBehaviour
     private void Start()
     {
         ManuallyPopulateChunks();
+        foreach(Chunk c in chunks)
+        {
+            Debug.Log(c.ChunkPosition);
+        }
         LoadNearbyChunks();
     }
     void ManuallyPopulateChunks()   //A debug method to manually create chunks and add blocks into them
     {
+        for(int i = -1; i <= 1; i++)
+        {
+            for(int j = -1; j <= 1; j++)
+            {
+                if (i == 0 && j == 0)
+                    continue;           //Skip the middle - a rigid platform is in place instead
 
+                Chunk newChunk = new Chunk();
+                newChunk.ChunkPosition = new Vector3(i * 32, j * 32, 0);
+
+                //Create a stone layer in the middle of the chunk
+                for(int x = 0; x < newChunk.ChunkBlockIDs.GetLength(0); x++)
+                {
+                    for (int y = 0; y < newChunk.ChunkBlockIDs.GetLength(1); y++)
+                    {
+                        newChunk.ChunkBlockIDs[x, y, (newChunk.ChunkBlockIDs.GetLength(2)) / 2] = 1;
+                    }
+                }
+                chunks.Add(newChunk);
+            }
+        }
     }
     void LoadNearbyChunks() //Loads nearby chunks
     {
@@ -24,12 +48,12 @@ public class ChunkManager : MonoBehaviour
     {
         
     }
-    Chunk GetNearestChunk(Vector3 position)  //Returns the chunk that is closest to the input position
+    public Chunk GetChunkAtPosition(Vector3 position)  //Returns the chunk that is closest to the input position
     {
         Chunk closestChunk = null;
         float currentDistance, lastDistance;
         lastDistance = 0;
-        foreach(Chunk c in chunks)
+        foreach (Chunk c in chunks)
         {
             currentDistance = Vector3.Distance(position, c.ChunkPosition);      //cache the calculated distance to offload work from the cpu
             if (currentDistance < lastDistance)
@@ -39,5 +63,28 @@ public class ChunkManager : MonoBehaviour
             }
         }
         return closestChunk;
+    }
+    public int[] GetBlockAtPosition(Vector3 position)      //Returns the chunk coordinates of a block closest to the input position [chunk ID in the chunks list, block x, block y, block z]
+    {
+        int[] result = new int[4];
+
+        int blockChunkID = chunks.IndexOf(GetChunkAtPosition(position));    //ID of the chunk that the block is in
+
+        Vector3 localPosition = position - chunks[blockChunkID].ChunkPosition;  
+        int blockX = Mathf.RoundToInt(localPosition.x);
+        int blockY = Mathf.RoundToInt(localPosition.y);
+        int blockZ = Mathf.RoundToInt(localPosition.z);
+
+        result[0] = blockChunkID;
+        result[1] = blockX;
+        result[2] = blockY;
+        result[3] = blockZ;
+
+        return result;
+    }
+    public Vector3 GetBlockPosition(int chunkID, Vector3 blockCoordinates) //Returns global coordinates of a block in a chunk at the specified coordinates
+    {
+        Vector3 blockPosition = blockCoordinates + chunks[chunkID].ChunkPosition;
+        return blockPosition;
     }
 }
